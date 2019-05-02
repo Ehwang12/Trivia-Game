e6d7235e9958a641349056227fddc697c9081dec
Esther Hwang <estshwang@gmail.com>
2019-05-02 17:34:00 -0500

    restyled base html from buttons to radio buttons, prelim timer working, button event listener currently being built

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,26 +2,11 @@ $(function(){
 //variables for win and loss tracker
 let win = 0;
 let loss = 0;
+let unanswered = 0;
 
 //our questions in an array
 //change the answer format so it only requires on value
-let questionsArray = [
-    {question:"What does Amity mean?", 
-     options:"a) friendship", 
-     answer:"The answer is friendship! It's always friendship..."},
-    {question:"\"That's some bad hat, Harry\"-Who says this line?", 
-     options:"d) Chief Brody", 
-     answer:"The answer is Chief Brody (someone's grumpy)."},
-    {question:"What song does Quint always sing?",
-     options:"b) Ladies from Maine",
-     answer:"The answer is Ladies of Spain." },
-    {question:"Mrs. Kintner sets a bounty of ___________ to catch the shark.",
-     options:"b) $3000",
-     answer:"The answer is $3000. That's a lotta change!"  },
-    {question:"What was the loving nickname given to the mechanical shark used in the film?",
-        options:"c) Bruce",
-        answer:"The answer is Bruce, named after Steven Spieldberg's lawyer." }
-]
+
 
 
 //start game function
@@ -38,48 +23,63 @@ function startGame() {
 
 }
 
-//+++++++++++ Question Generator ++++++++++++++//
+//+++++++++++ Button Assignment ++++++++++++++//
+$("button").on("click", function() {
+    //if wrong button clicked then 
+    if($("#wrong") === 0) {
+        //track loss
+        loss++;
+        $()
+    }
+        
+    //if no button clicked then
+        //track unanswered
+    //if correct button clicked then
+        //track wins
 
-//for loop through 'questions' array 
-for(let i = 0; i<questionsArray.length; i++) {
-    //each time you go through loop select question value from array
-    let question = questionsArray.question[i];
-    console.log(question);
-    //create and append to question div
-}
+
+})
+
    
 
-    //+++++ to create answer buttons +++++++++//
-        //each time through loop grab each comma section and create a button out of each element
-        //or alternatively assign the option comma sections to preconstructed buttons?
 
 
 
 
 
 
+//++++++++++++ Page Timer ++++++++++++++++++//
+//60 second timer counting down
+var counter = 60;
+var intervalId;
 
-//++++++++++++ Page Setup ++++++++++++++++++//
+function start() {
+    stop();
+    intervalId = setInterval(decrement, 1000);
+}
 
-//each page has a 30 second timer
+function decrement() {
+    
+    counter--;
 
-//if the player chooses right answer then...
-    //the next card will show with the 'correct' card
-    //win tracker updates ++
-    //congratulatory gif 
-    //moves to the next card after 30 seconds
+    $("#timer").html("<h3>" + counter + "</h3>");
+    
+    if(counter === 0) {
+        stop();
+        console.log("stop");
+        //go to score tracker card
+    }
 
-//else if player chooses wrong card then...
-    //the next card will show with the 'incorrect' card
-    //loss tracker updates ++
-    //disappointment gif
-    //moves to the next card after 30 seconds
+}
 
-//else if player puts in no input || the timer runs out then...
-    //the next card will show with the 'incorrect' card
-    //unanswered tracker updates ++
-    //disappointment gif
-    //moves to the next card after 30 seconds
+function stop() {
+    
+    clearInterval(intervalId);
+}
+
+start();
+
+
 
 
 
