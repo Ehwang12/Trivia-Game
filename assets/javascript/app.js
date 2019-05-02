d5bfa5e45a9f1441604b13cc6f0c752c92d134c6
Esther Hwang <estshwang@gmail.com>
2019-05-02 00:25:29 -0500

    wrote in some pseudo code

---
+++ assets/javascript/app.js
@@ -0,0 +1,76 @@
+$(function(){
+//variables for win and loss tracker
+let win = 0;
+let loss = 0;
+
+//our questions in an array
+let questionsArray = [
+    {question:"What does Amity mean?", 
+     options:"a) friendship", "b) ill-will", "c) A miracle", "d) A woman's name",
+     answer:"The answer is friendship!"},
+    {question:"\"That's some bad hat, Harry\"-Who says this line?",
+     options:"a) Larry Vaughn", "b) Matt Hooper", "c) Sam Quint", "d) Chief Brody",
+     answer:"The answer is Chief Brody (someone's grumpy)."},
+    {question:  ,
+     options:  ,
+     answer:  },
+    {question:  ,
+     options:  ,
+     answer:  }
+]
+
+
+//start game function
+function startGame() {
+//initiate start page
+    //event listener for start button
+    //if clicked start game
+
+//reset array for loop
+
+}
+
+//+++++++++++ Question Generator ++++++++++++++//
+
+//for loop through 'questions' array 
+    //each time you go through loop select question value from array
+    //create and append to question div
+
+    //+++++ to create answer buttons +++++++++//
+        //each time through loop grab each comma section and create a button out of each element
+        //or alternatively assign the option comma sections to preconstructed buttons?
+
+
+
+
+
+
+
+
+//++++++++++++ Page Setup ++++++++++++++++++//
+
+//each page has a 30 second timer
+
+//if the player chooses right answer then...
+    //the next card will show with the 'correct' card
+    //win tracker updates ++
+    //congratulatory gif 
+    //moves to the next card after 30 seconds
+
+//else if player chooses wrong card then...
+    //the next card will show with the 'incorrect' card
+    //loss tracker updates ++
+    //disappointment gif
+    //moves to the next card after 30 seconds
+
+//else if player puts in no input || the timer runs out then...
+    //the next card will show with the 'incorrect' card
+    //unanswered tracker updates ++
+    //disappointment gif
+    //moves to the next card after 30 seconds
+
+
+
+
+
+})
