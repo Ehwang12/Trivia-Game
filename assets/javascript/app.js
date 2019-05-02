42ace202324fe7781b6894a30c88728127a871a3
Esther <estshwang@gmail.com>
2019-05-02 15:45:47 -0500

    added buttons, placeholder spaces for timer and question for html, some code implemented for js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,19 +4,23 @@ let win = 0;
 let loss = 0;
 
 //our questions in an array
+//change the answer format so it only requires on value
 let questionsArray = [
     {question:"What does Amity mean?", 
-     options:"a) friendship", "b) ill-will", "c) A miracle", "d) A woman's name",
-     answer:"The answer is friendship!"},
-    {question:"\"That's some bad hat, Harry\"-Who says this line?",
-     options:"a) Larry Vaughn", "b) Matt Hooper", "c) Sam Quint", "d) Chief Brody",
+     options:"a) friendship", 
+     answer:"The answer is friendship! It's always friendship..."},
+    {question:"\"That's some bad hat, Harry\"-Who says this line?", 
+     options:"d) Chief Brody", 
      answer:"The answer is Chief Brody (someone's grumpy)."},
-    {question:  ,
-     options:  ,
-     answer:  },
-    {question:  ,
-     options:  ,
-     answer:  }
+    {question:"What song does Quint always sing?",
+     options:"b) Ladies from Maine",
+     answer:"The answer is Ladies of Spain." },
+    {question:"Mrs. Kintner sets a bounty of ___________ to catch the shark.",
+     options:"b) $3000",
+     answer:"The answer is $3000. That's a lotta change!"  },
+    {question:"What was the loving nickname given to the mechanical shark used in the film?",
+        options:"c) Bruce",
+        answer:"The answer is Bruce, named after Steven Spieldberg's lawyer." }
 ]
 
 
@@ -28,18 +32,26 @@ function startGame() {
 
 //reset array for loop
 
+//+++++++++++++ Timer Setup +++++++++++++++++++++++//
+//setInterval for 30 seconds
+    //move on to next card after time is up
+
 }
 
 //+++++++++++ Question Generator ++++++++++++++//
 
 //for loop through 'questions' array 
+for(let i = 0; i<questionsArray.length; i++) {
     //each time you go through loop select question value from array
+    let question = questionsArray.question[i];
+    console.log(question);
     //create and append to question div
+}
+   
 
     //+++++ to create answer buttons +++++++++//
         //each time through loop grab each comma section and create a button out of each element
         //or alternatively assign the option comma sections to preconstructed buttons?
-
 
 
 
