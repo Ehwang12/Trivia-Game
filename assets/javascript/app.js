cd18ab522d02bd919c5d76a29ba65c9b2b00dda9
Esther Hwang <estshwang@gmail.com>
2019-05-02 21:02:52 -0500

    managed to hide container when time runs out, still can't figure out how to account for unchecked, able to console log wins and losses

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,7 +15,7 @@ var answer5;
 
 
 //start game function
-function startGame() {
+
 
     //reset player score
       //win = 0;
@@ -26,12 +26,68 @@ function startGame() {
       //uncheck answers
 
     //reset timer
+      //counter = 60;
     
+
+
+//++++++++++++ Page Timer ++++++++++++++++++//
+//60 second timer counting down
+var counter = 10;
+var intervalId;
+
+
+
+
+
+function start() {
+    stop();
+    intervalId = setInterval(decrement, 1000);
+    }
+
+function decrement() {
+    
+    counter--;
+
+    $("#timer").html("<h3>" + counter + "</h3>");
+    
+    if(counter === 0) {
+        stop();
+        $(".questionsContainer").hide();
+       
+        //go to score tracker card
+        scoreCard();
+        }
+
+    }
+
+function stop() {
+    
+    clearInterval(intervalId);
+    }
+
+
+start();
+
+function scoreCard() {
+    //create div to put elements in
+    let scoreCard = $("<div class='row'>");
+    let winsDiv = $("<div class='col-12 text-center'>");
+    
+    scoreCard.append(winsDiv);
+        
+    //put wins in element
+    let wins = winsDiv.append("<h1>Wins: " + win + "</h1>");
+    //append win score to DOM
+    $(".questionsContainer").append(wins);
+
+
 }
+
+
 
 //+++++++++++ Button Assignments ++++++++++++++//
 
-//if wrong answer clicked
+//button event handler
 $("#submit").on("click", function(){
     answer1 = $("#correct1");
     answer2 = $("#correct2");
@@ -44,8 +100,9 @@ $("#submit").on("click", function(){
        win++;
        
    } else {
-       loss++;
        
+        loss++;
+
    }
 
    if(answer2.is(':checked')) {
@@ -83,60 +140,23 @@ $("#submit").on("click", function(){
        
    }
 
-   if ($(".btn").checked === true) {
-       console.log("sucks");
-   }
-   console.log("wins: " + win + " Loss: " + loss + " unanswered " + unanswered);
-   //switch statement? for answers?
-   
-   });
+   $(".questionsContainer").hide();
+
+   stop();
+
+   scoreCard();
+  
+});
 
 
 
 
 //if no answers clicked:
+   //unanswered++;
+   //update counter;
+   //display counter;
 
 
-
-
-function generateQuiz() {
-
-}
-
-
-
-
-
-//++++++++++++ Page Timer ++++++++++++++++++//
-//60 second timer counting down
-var counter = 60;
-var intervalId;
-
-function start() {
-    stop();
-    intervalId = setInterval(decrement, 1000);
-}
-
-function decrement() {
-    
-    counter--;
-
-    $("#timer").html("<h3>" + counter + "</h3>");
-    
-    if(counter === 0) {
-        stop();
-        console.log("stop");
-        //go to score tracker card
-    }
-
-}
-
-function stop() {
-    
-    clearInterval(intervalId);
-}
-
-start();
 
 
 
